01e24a08924cef320079e824c97c407229da1f2d
ankithshetty17 <ankishetty2002@gmail.com>
2024-10-22 18:46:09 +0530

    initial-commit

---
+++ lib/presentation/splash/pages/splash_screen.dart
@@ -0,0 +1,18 @@
+import 'package:ecommerce/core/themes/app_colors.dart';
+import 'package:flutter/material.dart';
+
+
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Center(
+          
+      ),
+    );
+  }
+}
